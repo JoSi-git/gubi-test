06e28b676dd3b38d717bb9d4f07e5a3820686d33
arkaizn <silas.gubler@hispeed.ch>
2025-05-07 12:09:23 +0200

    add volume folders

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# create volume folders
+mkdir ./scripts/Docker/moodle
+mkdir ./scripts/Docker/moodledata
+mkdir ./scripts/Docker/db_data
+
 
 git clone -b MOODLE_403_STABLE https://github.com/moodle/moodle.git ./moodle
 
 cd ./Docker
 docker compose up -d
+
