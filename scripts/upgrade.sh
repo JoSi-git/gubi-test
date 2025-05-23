142f71f863bc488a7851ef1c6247aa794e2dd9e5
arkaizn <silas.gubler@hispeed.ch>
2025-05-23 20:20:28 +0200

    pushi

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,1 +1,6 @@
 docker exec -u www-data moodle-migration php /var/www/html/admin/cli/upgrade.php --non-interactive
+# docker compose down
+
+# docker image git clone moodle version ändern mit sed
+
+# docker image neu builden
